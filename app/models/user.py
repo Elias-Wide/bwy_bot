67f40bf728b9e6c3931c071b6a22a0bc4f92aaaf
Krasikoff <126888377+Krasikoff@users.noreply.github.com>
2024-04-06 15:05:46 +0300

    Update user.py
    annotation Self to TypeVar

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,13 @@
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from typing import TypeVar
+
 
 from app.core.db import Base
+
+Self = TypeVar("Self", bound="User")
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
 
-    def __str__(self: isinstance) -> str:
+    def __str__(self: Self) -> str:
         return f' #{self.id}  {self.email}'
