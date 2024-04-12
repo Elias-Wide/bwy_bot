8536f66ad7a0d43463f29826c787157d0862d95c
mign0n <mign0n.git@gmail.com>
2024-04-12 15:20:25 +0500

    feat: Add a questionnaire feature.

--- app/handlers/__init__.py
+++
@@ -1,2 +0,0 @@
-from .survey import router as survey_router
-from .user_handlers import router as trainings_router
--- app/handlers/callbacks/__init__.py
+++ app/handlers/callbacks/__init__.py
@@ -1,3 +1,3 @@
 from .other_handler import router as echo_router
-from .survey import router as survey_router
+from .survey_handlers import router as survey_router
 from .user_handlers import router as user_router
