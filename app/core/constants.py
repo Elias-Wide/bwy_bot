de2bb42b6e259dfce8cc703009a16f8439d52272
Алёна Перова <perovaai31@yandex.ru>
2024-04-18 14:15:32 +0300

    conflict with develop

--- app/core/constants.py
+++ app/core/constants.py
@@ -52,6 +52,10 @@ WORKOUT_TYPE = (
 
 INTRO_SURVEY_TEXT = '<b>Здесь будет мотивирующие введение.</b>\n😜\n'
 
+ALLOWED_AGE_RANGE = (14, 55)
+ALLOWED_HEIGHT_RANGE = (100, 250)
+ALLOWED_WEIGHT_RANGE = (20, 500)
+
 PHYS_ACTIV_KOEF = {
     'ABSENT': 1.2,
     'INFREQUENT': 1.375,
--- app/handlers/callbacks/user_handlers.py
+++ app/handlers/callbacks/user_handlers.py
@@ -7,7 +7,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.logging import get_logger
 from app.crud.user import user_crud
-from app.crud.user import user_crud
 from app.handlers.menu_processor import get_menu_content
 from app.handlers.states import SurveyOrder
 from app.keyboards import MenuCallBack
@@ -19,16 +18,11 @@ logger = get_logger(__name__)
 
 
 @router.message(CommandStart(), SurveyOrder.finished)
-async def process_start_command(message: Message, state: FSMContext) -> None:
+async def process_start_command(message: Message,
+                                state: FSMContext,
+                                session: AsyncSession) -> None:
     """Хэндлер команды '/start'."""
-    user = await user_crud.get_by_attribute('telegram_id',
-                                            message.chat.id,
-                                            session)
-    media, reply_markup = await get_menu_content(
-        level=0,
-        menu_name='main',
-        user=user,
-        session=session)
+
     user = await user_crud.get_by_attribute('telegram_id',
                                             message.chat.id,
                                             session)
@@ -42,7 +36,6 @@ async def process_start_command(message: Message, state: FSMContext) -> None:
         caption=media.caption,
         reply_markup=reply_markup,
     )
-    logger.info(user)
     await state.clear()
 
 
@@ -51,19 +44,13 @@ async def user_menu(
     callback: CallbackQuery,
     callback_data: MenuCallBack,
     session: AsyncSession,
-    session: AsyncSession,
 ) -> None:
-    user = await user_crud.get_by_attribute('telegram_id',
-                                            callback.from_user.id,
-                                            session)
     user = await user_crud.get_by_attribute('telegram_id',
                                             callback.from_user.id,
                                             session)
     media, reply_markup = await get_menu_content(
         level=callback_data.level,
         menu_name=callback_data.menu_name,
-        user=user,
-        session=session,
         user=user,
         session=session,
     )
