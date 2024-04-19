7f16488e9698cc57b0b926c68abd0055aa5363f6
Krasikoff <z1r0-d@yandex.ru>
2024-04-19 10:28:54 +0300

    fix: alembic migration.

--- alembic/versions/634b7fd3a5cc_change_field_name_description.py
+++ alembic/versions/634b7fd3a5cc_change_field_name_description.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '634b7fd3a5cc'
-down_revision: Union[str, None] = 'c464c4d263c4'
+down_revision: Union[str, None] ='c464c4d263c4'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
