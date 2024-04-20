d7ee6eef68d87d011a28a6d253d86c5660b810c2
Krasikoff <126888377+Krasikoff@users.noreply.github.com>
2024-04-20 14:17:47 +0300

    Update constants.py
    Настройки -> Напоминание

--- app/core/constants.py
+++ app/core/constants.py
@@ -37,7 +37,7 @@ BUTTONS = {
     'Сон💤': 'sleep',
     'Питание🥦': 'diet',
     'Тренировки🏋‍♂️': 'workouts',
-    'Настройки🛠': 'settings',
+    'Напоминания🛠': 'settings',
 }
 BUTTONS_FOR_TRAINING = {
     'category': {
